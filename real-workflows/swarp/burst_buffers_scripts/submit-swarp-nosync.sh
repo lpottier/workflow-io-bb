a62f2fe98f258dac7b4d3f80596a5e5d296ed530
Loïc Pottier <lpottier@isi.edu>
2019-10-01 12:57:51 -0700

    scripts to run swarp on BB without sync
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

---
+++ real-workflows/swarp/burst_buffers_scripts/submit-swarp-nosync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -x
+for i in 1; do
+#for i in 4; do
+#for i in 2 16 32; do
+#for i in 1 2 4 32 64; do
+#for i in 8 16 64; do
+#for i in 1 2 4 8; do
+#for i in 16 32 64; do
+    outdir=$(mktemp --directory --tmpdir=$(/bin/pwd) swarp-run-${i}N.XXXXXX)
+    script="run-swarp-scaling-bb-nosync-${i}N.sh"
+    echo $outdir
+    echo $script
+    sed "s/@NODES@/${i}/" "run-swarp-scaling-bb-nosync.sh" > ${outdir}/${script}
+    for j in $(seq ${i} -1 1); do
+        stage_in="#DW stage_in source=/global/cscratch1/sd/lpottier/workflow-io-bb/real-workflows/swarp/input destination=\$DW_JOB_STRIPED/input/${j} type=directory"
+        sed -i "s|@STAGE@|@STAGE@\n${stage_in}|" ${outdir}/${script}
+    done
+    cp "bbinfo.sh" "${outdir}"
+    cd "${outdir}"
+    sbatch ${script}
+    cd ..
+done
+
