b0d1443471bd45485406658490430bf7f8f55254
Loic Pottier <lpottier@isi.edu>
2019-10-03 19:45:18 -0700

    ptraj wrapper

--- real-workflows/sns-workflow/bin/ptraj.sh
+++ real-workflows/sns-workflow/bin/ptraj.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -Eexo pipefail
 
-module unload namd
+#module unload namd
 module swap craype-${CRAY_CPU_TARGET} craype-haswell
-#module load namd
-
-if [ -z "${PEGASUS_CORES}" ]; then
-	PEGASUS_CORES=1
-fi
-
-#echo "$@"
-
+module load amber
 
 if [ ! -z "${PEGASUS_SCRATCH_DIR}" ]; then
         cd $PEGASUS_SCRATCH_DIR
 fi
 
-srun /global/homes/l/lpottier/workflow-io-bb/real-workflows/sns-workflow/bin/namd2 "$@"
+srun cpptraj "$@"
 
