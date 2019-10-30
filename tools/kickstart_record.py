73726d74e9a41fd390a2a5895f3a3f2e9e0a57f1
Loïc Pottier <lpottier@isi.edu>
2019-10-29 17:38:54 -0700

    temp xml parsing
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- tools/kickstart_record.py
+++ tools/kickstart_record.py
@@ -5,14 +5,40 @@ import xml.etree.ElementTree as xml
 from enum import Enum,unique,auto
 from pathlib import Path
 
+XML_PREFIX="{http://pegasus.isi.edu/schema/invocation}"
+
 @unique
 class FileType(Enum):
     XML = auto()
     YAML = auto()
 
+# class Job():
+#     """
+#     Job statistics from KickStart record 
+#     """
+#     def __init__(self, file, pid, hashmap):
+#         self.file       =       file
+#         self.pid        =       pid
+
+#         # Hashmap must be the attrib from <usage> elem
+#         self.utime      =       hashmap["utime"]
+#         self.stime      =       hashmap["stime"]
+#         self.maxrss     =       hashmap["maxrss"]
+#         self.minflt     =       hashmap["minflt"]
+#         self.majflt     =       hashmap["majflt"]
+#         self.nswap      =       hashmap["nswap"]
+#         self.inblock    =       hashmap["inblock"]
+#         self.outblock   =       hashmap["outblock"]
+#         self.msgsnd     =       hashmap["msgsnd"]
+#         self.msgrcv     =       hashmap["msgrcv"]
+#         self.nsignals   =       hashmap["nsignals"] 
+#         self.nvcsw      =       hashmap["nvcsw"]
+#         self.nivcsw     =       hashmap["nivcsw"]
+
+
 class KickstartRecord(object):
     """
-    docstring for KickstartRecord
+    KickstartRecord
     """
     def __init__(self, path, file_type=FileType.XML):
         super(KickstartRecord, self).__init__()
@@ -29,7 +55,11 @@ class KickstartRecord(object):
             raise ValueError("{} is not a regular file.".format(path))
         self._ftype = file_type
 
-        self._attr = {}
+        self._exec = {}
+        self._exec["files"] = []
+        self._stdout = []
+        self._jobs = []                  #Multiple jobs?
+        self._uname = {}
 
     def path(self):
         return self._path
@@ -37,40 +67,74 @@ class KickstartRecord(object):
     def stat(self):
         return self._path.stat()
 
+    # @staticmethod
+    # def findall_and_store(xml_root, elem, hashmap, key):
+    #     # Store information about the machine
+    #     target = XML_PREFIX+elem
+    #     targets_found = xml_root.findall(target)
+
+    #     if not targets_found:
+    #         print ("{} not found, or it has no subelements".format(target))
+    #     if targets_found is None:
+    #         print ("{} not found".format(target))
+
+    #     for elem in targets_found:
+    #         for e in elem.iter():
+    #             if e.tag == XML_PREFIX+key:
+    #                 hashmap[key] = (e.text, e.attrib)
+
     def parse(self):
-        tree = xml.parse(self._path)
+        tree = xml.ElementTree()
+        tree.parse(self._path)
         root = tree.getroot()
-        for child in root:
-            print(child.tag, " ", child.attrib)
-        print("===")
+        for elem in root.iter():
+            if elem.tag == XML_PREFIX+"file" and elem.attrib['name'] != None:
+                self._exec["files"].append(elem.attrib['name'])
+            if elem.tag == XML_PREFIX+"cwd":
+                self._exec["cwd"] = elem.text
+            if elem.tag == XML_PREFIX+"uname":
+                self._uname = elem.attrib
+                self._uname['text']=elem.text
 
-        # Store machine information
-        machines = root.findall("{http://pegasus.isi.edu/schema/invocation}machine")
-        if not machines:  # careful!
-            print ("{http://pegasus.isi.edu/schema/invocation}machine not found, or element has no subelements")
+            if elem.tag == XML_PREFIX+"data":
+                self._stdout += [elem.text]
 
-        if machines is None:
-            print ("{http://pegasus.isi.edu/schema/invocation}machine not found")
+        print (root.findall('.//usage...'))
 
-        for machine in machines:
-            for elem in machine.iter():
-                if elem.tag == "{http://pegasus.isi.edu/schema/invocation}uname":
-                    self._attr["uname"] = (elem.text, elem.attrib)
+                #self._job.(Job(file, pid, hashmap)
+
+            # print(" tag=",elem.tag, " \ntext=", elem.text ," \ntail=", elem.tail, " \nattrib=", elem.attrib)
+            # print("==========================================================================================")
+
+        # 'usage' nodes that are children of nodes with name='invocation' (the root node)
+        #root.findall(".//*[@name='invocation']/usage")
+        print(tree.getroot().findall("invocation"))
+        for elem in root.findall("invocation/usage"):
+            print(" tag=",elem.tag, " \ntext=", elem.text ," \ntail=", elem.tail, " \nattrib=", elem.attrib)
+            print("==========================================================================================")
 
 
-        cwd = root.findall("{http://pegasus.isi.edu/schema/invocation}cwd")
-        for command in root.findall("{http://pegasus.isi.edu/schema/invocation}cwd"):
-            for elem in command.iter():
-                self._attr["cwd"] = (elem.text, elem.attrib)
+        # # Store information about the machine
+        # KickstartRecord.findall_and_store(root, "machine",  self._attr, "uname")
+        # KickstartRecord.findall_and_store(root, "cwd",  self._attr, "cwd")
 
-        for data in root.findall("{http://pegasus.isi.edu/schema/invocation}data"):
-            for elem in data.iter():
-                print(elem.tag, " ", elem.text, " ", elem.attrib)
-                #self.data["cwd"] = (elem.text, elem.attrib)
+        # print("===")
 
-        # for statcall in root.findall("{http://pegasus.isi.edu/schema/invocation}machine"):
-        #     for child in statcall:
-        #         print(child.tag, " ", elem.text, " ", child.attrib)
+        # for data in root.findall(XML_PREFIX+"data"):
+        #     #for elem in data.iter():
+        #     print(data.tag, " ", data.tail, " ", data.attrib)
+        #         #self._attr["data"] = (elem.tail, elem.attrib)
+
+        # # for statcall in root.findall("{http://pegasus.isi.edu/schema/invocation}machine"):
+        # #     for child in statcall:
+        # #         print(child.tag, " ", elem.text, " ", child.attrib)
+
+        #print(self._attr)
+
+        print(self._exec)
+        print(self._uname)
+        print(self._stdout)
+        #print(self._job)
 
 
 if __name__ == "__main__":
