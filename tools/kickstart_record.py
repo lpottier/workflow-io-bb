dabd3326766f4e45ef3d3a10d537e20554559e2e
Loïc Pottier <lpottier@isi.edu>
2019-11-01 17:18:41 -0700

    added processus xml parsing
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- tools/kickstart_record.py
+++ tools/kickstart_record.py
@@ -77,7 +77,7 @@ class Machine:
         for name,val in vars(self).items():
             if val == {}:
                 val = "Not found"
-            s = s + '{:<8} -> {:<72}\n'.format(name, str(val))
+            s = s + '{:<10} -> {:<72}\n'.format(name, str(val))
         return s
 
     def __repr__(self):
@@ -85,7 +85,7 @@ class Machine:
         for name,val in vars(self).items():
             if val == {}:
                 val = "Not found"
-            s = s + '{:<8} -> {:<72}\n'.format(name, str(val))
+            s = s + '{:<10} -> {:<72}\n'.format(name, str(val))
         return s
 
 class File:
@@ -123,63 +123,112 @@ class File:
 
 class Processus:
     """
-    Job statistics from KickStart record 
+    Job statistics from KickStart record
+
+    <proc ppid="14774" pid="14775" exe="/bin/bash" start="1571940058.795641" 
+        stop="1571940058.796241" utime="0.000" stime="0.000" iowait="0.000" 
+        finthreads="1" maxthreads="0" totthreads="0" vmpeak="12144" rsspeak="2664" 
+        rchar="0" wchar="100" rbytes="0" wbytes="0" cwbytes="0" syscr="0" syscw="3">
+
+        <file name="/dev/null" bread="0" nread="0" bwrite="0" nwrite="0" bseek="0" nseek="0" size="0"/>
+        <file name="/tmp/ks.out.fRUVe3" bread="0" nread="0" bwrite="0" nwrite="0" bseek="0" nseek="0" size="0"/>
+        <file name="/tmp/ks.err.vP02l5" bread="0" nread="0" bwrite="0" nwrite="0" bseek="0" nseek="0" size="0"/>
+        <file name="pipe:[8212470]" bread="0" nread="0" bwrite="100" nwrite="3" bseek="0" nseek="0" size="0"/>
+        <file name="/dev/kgni0" bread="0" nread="0" bwrite="0" nwrite="0" bseek="0" nseek="0" size="0"/>
+    </proc>
+
     """
-    def __init__(self, hashmap):
-        self.pid        =       hashmap["pid"]
-        self.ppid       =       hashmap["ppid"]
+    def __init__(self, xml_root):
+
+        if xml_root.tag != XML_PREFIX+"proc":
+            raise ValueError("Nota {} ElementTree {}".format("proc",element_tree.tag))
+       
+        self.pid        =       xml_root.attrib["pid"]
+        self.ppid       =       xml_root.attrib["ppid"]
 
         # Hashmap must be the attrib from <proc> elem
-        self.exe        =       hashmap["exe"]
-        self.start      =       hashmap["start"]
-        self.stop       =       hashmap["stop"]
-        self.utime      =       hashmap["utime"]
-        self.stime      =       hashmap["stime"]
-        self.iowait     =       hashmap["iowait"]
-        self.finthreads =       hashmap["finthreads"]
-        self.maxthreads =       hashmap["maxthreads"]
-        self.totthreads =       hashmap["totthreads"]
-        self.vmpeak     =       hashmap["vmpeak"]
-        self.rsspeak    =       hashmap["rsspeak"]
-        self.rchar      =       hashmap["rchar"]
-        self.wchar      =       hashmap["wchar"]
-        self.rbytes     =       hashmap["rbytes"]
-        self.wbytes     =       hashmap["wbytes"]
-        self.cwbytes    =       hashmap["cwbytes"]
-        self.syscr      =       hashmap["syscr"]
-        self.syscw      =       hashmap["syscw"]
+        self.exe        =       xml_root.attrib["exe"]
+        self.start      =       xml_root.attrib["start"]
+        self.stop       =       xml_root.attrib["stop"]
+        self.utime      =       xml_root.attrib["utime"]
+        self.stime      =       xml_root.attrib["stime"]
+        self.iowait     =       xml_root.attrib["iowait"]
+        self.finthreads =       xml_root.attrib["finthreads"]
+        self.maxthreads =       xml_root.attrib["maxthreads"]
+        self.totthreads =       xml_root.attrib["totthreads"]
+        self.vmpeak     =       xml_root.attrib["vmpeak"]
+        self.rsspeak    =       xml_root.attrib["rsspeak"]
+        self.rchar      =       xml_root.attrib["rchar"]
+        self.wchar      =       xml_root.attrib["wchar"]
+        self.rbytes     =       xml_root.attrib["rbytes"]
+        self.wbytes     =       xml_root.attrib["wbytes"]
+        self.cwbytes    =       xml_root.attrib["cwbytes"]
+        self.syscr      =       xml_root.attrib["syscr"]
+        self.syscw      =       xml_root.attrib["syscw"]
 
+        self.files = {} #key is name and value a File object
+        self.parse_files(xml_root)
 
-class Usage:
-    """
-    Usage statistics from KickStart record 
-    """
-    def __init__(self, hashmap):
-        # Hashmap must be the attrib from <usage> elem
-        self.utime      =       hashmap["utime"]
-        self.stime      =       hashmap["stime"]
-        self.maxrss     =       hashmap["maxrss"]
-        self.minflt     =       hashmap["minflt"]
-        self.majflt     =       hashmap["majflt"]
-        self.nswap      =       hashmap["nswap"]
-        self.inblock    =       hashmap["inblock"]
-        self.outblock   =       hashmap["outblock"]
-        self.msgsnd     =       hashmap["msgsnd"]
-        self.msgrcv     =       hashmap["msgrcv"]
-        self.nsignals   =       hashmap["nsignals"] 
-        self.nvcsw      =       hashmap["nvcsw"]
-        self.nivcsw     =       hashmap["nivcsw"]
+    def parse_files(self, xml_root):
+        for file in xml_root:
+            idx = file.attrib["name"]
+            self.files[idx] = File(file.attrib)
 
     def __str__(self):
         s = ''
         for name,val in vars(self).items():
-            s = s + '{:<8} -> {:>10}\n'.format(name, str(val))
+            if name == "files":
+                continue
+            s = s + '{:<10} -> {:<10}\n'.format(name, str(val))
         return s
 
     def __repr__(self):
         s = ''
         for name,val in vars(self).items():
-            s = s + '{:<8} -> {:>10}\n'.format(name, str(val))
+            if name == "files":
+                continue
+            s = s + '{:<10} -> {:<10}\n'.format(name, str(val))
+        return s
+
+class Usage:
+    """
+    Usage statistics from KickStart record 
+    """
+    def __init__(self, xml_tree):
+        # Hashmap must be the attrib from <usage> elem
+        self.utime      =       xml_tree.attrib["utime"]
+        self.stime      =       xml_tree.attrib["stime"]
+        self.maxrss     =       xml_tree.attrib["maxrss"]
+        self.minflt     =       xml_tree.attrib["minflt"]
+        self.majflt     =       xml_tree.attrib["majflt"]
+        self.nswap      =       xml_tree.attrib["nswap"]
+        self.inblock    =       xml_tree.attrib["inblock"]
+        self.outblock   =       xml_tree.attrib["outblock"]
+        self.msgsnd     =       xml_tree.attrib["msgsnd"]
+        self.msgrcv     =       xml_tree.attrib["msgrcv"]
+        self.nsignals   =       xml_tree.attrib["nsignals"] 
+        self.nvcsw      =       xml_tree.attrib["nvcsw"]
+        self.nivcsw     =       xml_tree.attrib["nivcsw"]
+
+        # Data from statcall: stdin, stdout, stderr and metadata
+        self.data = {}
+        for elem in xml_tree:
+            if elem.tag == XML_PREFIX+"statcall":
+                self.data[elem.attrib["id"]] = None
+                for data in elem:
+                    if data.tag == XML_PREFIX+"data":
+                        self.data[elem.attrib["id"]] = data.text
+
+    def __str__(self):
+        s = ''
+        for name,val in vars(self).items():
+            s = s + '{:<10} -> {:<10}\n'.format(name, str(val))
+        return s
+
+    def __repr__(self):
+        s = ''
+        for name,val in vars(self).items():
+            s = s + '{:<10} -> {:<10}\n'.format(name, str(val))
         return s
 
 class KickstartRecord(object):
@@ -209,17 +258,23 @@ class KickstartRecord(object):
         print(self.machine)
 
         # Usage (global, not mainjob)
-        self.global_usage = Usage(self._get_elem("usage").attrib)
-        print(self.global_usage)
-
-        # Parse I/O, stdin, stdout, stderr and metadata
-        self._statcalls = {}
-        self._get_statcalls_data()
+        self.usage = Usage(self._get_elem("usage"))
+        print(self.usage)
 
         #Store all processes involved
-        self.processes = []
+        # key is pid and value a Processus object
+        # each processus object store a map of the files accessed
+        self.processes = {}
+        self.parse_proc(self._get_elem("mainjob"))
+
+        # print(list(self.processes.values())[0])
 
 
+    def parse_proc(self, xml_root):
+        for proc in xml_root:
+            if proc.tag == XML_PREFIX+"proc":
+                idx = proc.attrib["pid"]
+                self.processes[idx] = Processus(proc)
 
     def path(self):
         return self._path
@@ -227,61 +282,11 @@ class KickstartRecord(object):
     def stat(self):
         return self._path.stat()
 
-    # @staticmethod
-    # def findall_and_store(xml_root, elem, hashmap, key):
-    #     # Store information about the machine
-    #     target = XML_PREFIX+elem
-    #     targets_found = xml_root.findall(target)
-
-    #     if not targets_found:
-    #         print ("{} not found, or it has no subelements".format(target))
-    #     if targets_found is None:
-    #         print ("{} not found".format(target))
-
-    #     for elem in targets_found:
-    #         for e in elem.iter():
-    #             if e.tag == XML_PREFIX+key:
-    #                 hashmap[key] = (e.text, e.attrib)
-
-    def _get_statcalls_data(self):
-        for elem in self._root:
-            if elem.tag == XML_PREFIX+"statcall":
-                self._statcalls[elem.attrib["id"]] = None
-                for data in elem:
-                    if data.tag == XML_PREFIX+"data":
-                        self._statcalls[elem.attrib["id"]] = data.text
-
     def _get_elem(self, key):
         for elem in self._root:
             if elem.tag == XML_PREFIX+str(key):
                 return elem
         return None
-
-    # def parse(self):
-    #     tree = xml.ElementTree()
-    #     tree.parse(self._path)
-    #     root = tree.getroot()
-    #     for elem in root:
-    #         if elem.tag == XML_PREFIX+"usage":
-    #             self._job = elem.attrib
-    #         # print(" tag=",elem.tag, " \ntext=", elem.text ," \ntail=", elem.tail, " \nattrib=", elem.attrib)
-    #         # print("==========================================================================================")
-
-    #     for elem in root.iter():
-    #         if elem.tag == XML_PREFIX+"file" and elem.attrib['name'] != None:
-    #             self._exec["files"].append(elem.attrib['name'])
-    #         if elem.tag == XML_PREFIX+"cwd":
-    #             self._exec["cwd"] = elem.text
-    #         if elem.tag == XML_PREFIX+"uname":
-    #             self._uname = elem.attrib
-    #             self._uname['text']=elem.text
-
-    #     #print(self._exec)
-    #     print(self._uname)
-        
-    #     print(self._statcalls.keys())
-    #     print(self._job)
-
 
 if __name__ == "__main__":
     test_record = KickstartRecord("stat.resample.xml")
