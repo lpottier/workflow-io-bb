27d07fda09d6b59c88173dbb4c2da29754d4fd22
Loïc Pottier <lpottier@isi.edu>
2019-11-07 14:49:58 -0800

    added in start.sh the list of files to stage in
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- real-workflows/swarp/run/start.sh
+++ real-workflows/swarp/run/start.sh
@@ -2,4 +2,20 @@
 
 set -x
 
-salloc -N 1 -C haswell -q interactive -t 01:00:00 --bbf=run/bbf.conf
+BASE="/global/cscratch1/sd/lpottier/workflow-io-bb/real-workflows/swarp/"
+
+INPUT_DIR=${BASE}/input
+IMAGE_PATTERN='PTF201111*.w.fits'
+IMAGE_WEIGHT_PATTERN='PTF201111*.w.weight.fits'
+STAGE_FILES=files_to_stage.txt
+
+rm -f ${STAGE_FILES}
+
+for f in $(ls ${INPUT_DIR}/${IMAGE_PATTERN}); do
+    echo $f '$DW_JOB_STRIPED'/input >> ${STAGE_FILES}
+
+for f in $(ls ${INPUT_DIR}/${IMAGE_WEIGHT_PATTERN}); do
+    echo $f '$DW_JOB_STRIPED'/input >> ${STAGE_FILES}
+
+
+#salloc -N 1 -C haswell -q interactive -t 01:00:00 --bbf=bbf.conf
