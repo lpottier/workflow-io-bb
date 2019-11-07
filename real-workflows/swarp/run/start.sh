f7588a9b92aed9e6f78291d0c9ae7ccd00a4d453
Loïc Pottier <lpottier@isi.edu>
2019-11-07 14:52:27 -0800

    fixed errors
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- real-workflows/swarp/run/start.sh
+++ real-workflows/swarp/run/start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 BASE="/global/cscratch1/sd/lpottier/workflow-io-bb/real-workflows/swarp/"
 
 INPUT_DIR=${BASE}/input
 IMAGE_PATTERN='PTF201111*.w.fits'
 IMAGE_WEIGHT_PATTERN='PTF201111*.w.weight.fits'
-STAGE_FILES=files_to_stage.txt
+STAGE_FILE=files_to_stage.txt
 
 rm -f ${STAGE_FILES}
 
 for f in $(ls ${INPUT_DIR}/${IMAGE_PATTERN}); do
-    echo $f '$DW_JOB_STRIPED'/input >> ${STAGE_FILES}
+    echo $f '$DW_JOB_STRIPED'/input >> $STAGE_FILE
 done
 
 for f in $(ls ${INPUT_DIR}/${IMAGE_WEIGHT_PATTERN}); do
-    echo $f '$DW_JOB_STRIPED'/input >> ${STAGE_FILES}
+    echo $f '$DW_JOB_STRIPED'/input >> $STAGE_FILE
 done
 
 #salloc -N 1 -C haswell -q interactive -t 01:00:00 --bbf=bbf.conf
