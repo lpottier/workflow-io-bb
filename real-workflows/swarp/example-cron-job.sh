bcd958a1e956ca740d3a1d8481277db04cb23103
Loïc Pottier <lpottier@isi.edu>
2020-02-12 19:17:07 -0800

    added example of cron jobs

---
+++ real-workflows/swarp/example-cron-job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+source /global/homes/l/lpottier/.bashrc
+
+PROC=1
+AVG=1
+BB=50
+TIME="0:30:00"
+PIPELINE="1"
+FILES=32
+QUEUE="debug"
+
+DIR="/global/cscratch1/sd/lpottier/workflow-io-bb/real-workflows/swarp/"
+
+RUN="python3 $DIR/generate_scripts.py"
+
+#You must do `module save swarp-run` from the login node first !
+
+module restore swarp-run
+
+cd "$DIR/"
+
+### No Fits -> done
+$RUN -S -b $BB -p $PROC -w $PIPELINE -r $AVG -t $TIME -q $QUEUE -c $FILES
+
