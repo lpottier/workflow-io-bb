417dd7af4060efe1e9f11dd6a51153f85bab657f
Loïc Pottier <lpottier@isi.edu>
2020-02-17 19:09:03 -0800

    added multiple swarp dir managemnt
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- simulation/europar20-swarp.sh
+++ simulation/europar20-swarp.sh
@@ -47,101 +47,103 @@ cd ..
 
 echo "[$($DATE --rfc-3339=ns)] Done."
 
-KS_DIR_FILES="$PWD/data/trace-files/swarp/shared-cori/baseline-pfs/pipeline-1_cores-1/"
 MAIN_DIR="$PWD/data/trace-files/swarp/shared-cori/baseline-pfs/pipeline-1_cores-1/"
-SWARP_FOLDER="swarp-premium-32C-200B-1W-0F-2-2-private"
 
-EXP_DIR=$(echo $MAIN_DIR/$SWARP_FOLDER/swarp-run-*N-*F.*/swarp.*/)
-
-echo "[$($DATE --rfc-3339=ns)] Target directory found: $(basename $EXP_DIR)"
-echo ""
+SWARP_FOLDER="swarp-premium-32C-200B-1W-0F-2-2-private swarp-premium-32C-200B-1W-0F-2-2-private"
 
 print_header=''
 
-for run in $(find $EXP_DIR/* -maxdepth 0 -type d | sort -n); do
-    if (( "$VERBOSE" >= 1 )); then
-        echo "run found: $(basename $run)"
-    fi
-    for pipeline in $(find $run/* -maxdepth 0 -type d | sort -n); do
+for folder in $SWARP_FOLDER; do
+
+    EXP_DIR=$(echo $MAIN_DIR/$folder/swarp-run-*N-*F.*/swarp.*/)
+    echo "[$($DATE --rfc-3339=ns)] processing: $(basename $EXP_DIR)"
+    echo ""
+
+    for run in $(find $EXP_DIR/* -maxdepth 0 -type d | sort -n); do
         if (( "$VERBOSE" >= 1 )); then
-            echo "  pipeline found: $(basename $pipeline)"
+            echo "run found: $(basename $run)"
         fi
+        for pipeline in $(find $run/* -maxdepth 0 -type d | sort -n); do
+            if (( "$VERBOSE" >= 1 )); then
+                echo "  pipeline found: $(basename $pipeline)"
+            fi
 
-        LOC_OUTPUTLOG="$(find $run -maxdepth 1 -type f -name $OUTPUT_LOG)"
-        LOC_FILEMAP="$(find $pipeline -maxdepth 1 -type f -name $FILE_MAP)"
+            LOC_OUTPUTLOG="$(find $run -maxdepth 1 -type f -name $OUTPUT_LOG)"
+            LOC_FILEMAP="$(find $pipeline -maxdepth 1 -type f -name $FILE_MAP)"
 
-        LOC_STAGEIN="$(find $run -maxdepth 1 -type f -name $STAGEIN_CSV)"
-        LOC_RSMPL="$(find $pipeline -maxdepth 1 -type f -name $RSMPL*)"
-        LOC_COMBINE="$(find $pipeline -maxdepth 1 -type f -name $COMBINE*)"
+            LOC_STAGEIN="$(find $run -maxdepth 1 -type f -name $STAGEIN_CSV)"
+            LOC_RSMPL="$(find $pipeline -maxdepth 1 -type f -name $RSMPL*)"
+            LOC_COMBINE="$(find $pipeline -maxdepth 1 -type f -name $COMBINE*)"
 
-        if (( "$VERBOSE" >= 2 )); then
-            echo "  |-> $OUTPUT_LOG found: $(basename $LOC_OUTPUTLOG)"
-            echo "  |-> $FILE_MAP found: $(basename $LOC_FILEMAP)"
-            echo "  |-> $STAGEIN_CSV found: $(basename $LOC_STAGEIN)"
-            echo "  |-> $RSMPL found: $(basename $LOC_RSMPL)"
-            echo "  |-> $COMBINE found: $(basename $LOC_COMBINE)"
-        fi
+            if (( "$VERBOSE" >= 2 )); then
+                echo "  |-> $OUTPUT_LOG found: $(basename $LOC_OUTPUTLOG)"
+                echo "  |-> $FILE_MAP found: $(basename $LOC_FILEMAP)"
+                echo "  |-> $STAGEIN_CSV found: $(basename $LOC_STAGEIN)"
+                echo "  |-> $RSMPL found: $(basename $LOC_RSMPL)"
+                echo "  |-> $COMBINE found: $(basename $LOC_COMBINE)"
+            fi
 
-        err_daxgen="/dev/null"
-        if (( "$VERBOSE" >= 1 )); then
-            err_daxgen=1
-        fi
+            err_daxgen="/dev/null"
+            if (( "$VERBOSE" >= 1 )); then
+                err_daxgen=1
+            fi
 
-        DAX="$pipeline/${WORKFLOW%%.*}-$(basename $pipeline).dax"
-        ## Generate Pegasus DAX
-        $PYTHON $DAXGEN_DIR/daxgen.py \
-            --dax-file "$DAX" \
-            --scalability "$(basename $pipeline)" \
-            --stage-in > $err_daxgen 2>&1
+            DAX="$pipeline/${WORKFLOW%%.*}-$(basename $pipeline).dax"
+            ## Generate Pegasus DAX
+            $PYTHON $DAXGEN_DIR/daxgen.py \
+                --dax-file "$DAX" \
+                --scalability "$(basename $pipeline)" \
+                --stage-in > $err_daxgen 2>&1
 
 
-        ## Generate WRENCH DAX
-        if (( "$VERBOSE" >= 1 )); then
-            echo ""
-            echo "  [$($DATE --rfc-3339=ns)] Generate a WRENCH compatible DAX from $(basename $WORKFLOW)..."
-        fi
-        #DAX="$pipeline/${WORKFLOW%%.*}-$(basename $pipeline).dax"
+            ## Generate WRENCH DAX
+            if (( "$VERBOSE" >= 1 )); then
+                echo ""
+                echo "  [$($DATE --rfc-3339=ns)] Generate a WRENCH compatible DAX from $(basename $WORKFLOW)..."
+            fi
+            #DAX="$pipeline/${WORKFLOW%%.*}-$(basename $pipeline).dax"
 
-        err_ks_to_wrench="/dev/null"
-        if (( "$VERBOSE" >= 1 )); then
-            err_ks_to_wrench=1
-        fi
+            err_ks_to_wrench="/dev/null"
+            if (( "$VERBOSE" >= 1 )); then
+                err_ks_to_wrench=1
+            fi
 
-        #DAX="$pipeline/$WORKFLOW"
-        $PYTHON "$KS_TO_DAX" \
-            -x "$DAX" \
-            -k "$LOC_RSMPL" \
-            -k "$LOC_COMBINE" \
-            -i "$LOC_STAGEIN" \
-            -o "$DAX" 2>$err_ks_to_wrench
+            #DAX="$pipeline/$WORKFLOW"
+            $PYTHON "$KS_TO_DAX" \
+                -x "$DAX" \
+                -k "$LOC_RSMPL" \
+                -k "$LOC_COMBINE" \
+                -i "$LOC_STAGEIN" \
+                -o "$DAX" 2>$err_ks_to_wrench
 
-        if (( "$VERBOSE" >= 1 )); then
-            echo "  [$($DATE --rfc-3339=ns)] Done. Written in $DAX."
-            echo ""
+            if (( "$VERBOSE" >= 1 )); then
+                echo "  [$($DATE --rfc-3339=ns)] Done. Written in $DAX."
+                echo ""
 
-            echo "[$($DATE --rfc-3339=ns)] Run the simulations..."
-            echo ""
-        fi
+                echo "[$($DATE --rfc-3339=ns)] Run the simulations..."
+                echo ""
+            fi
 
-        $PWD/build/workflow-io-bb \
-            --id="$(basename $run)" \
-            --pipeline="$(basename $pipeline)" \
-            --platform="$PWD/data/platform-files/$PLATFORM" \
-            --dax="$DAX" "$print_header" \
-            --stage-file="$LOC_FILEMAP" \
-            --real-log="$LOC_OUTPUTLOG" \
-            --output="$pipeline" \
-            2> $pipeline/wrench.err
+            $PWD/build/workflow-io-bb \
+                --id="$(basename $run)" \
+                --pipeline="$(basename $pipeline)" \
+                --platform="$PWD/data/platform-files/$PLATFORM" \
+                --dax="$DAX" "$print_header" \
+                --stage-file="$LOC_FILEMAP" \
+                --real-log="$LOC_OUTPUTLOG" \
+                --output="$pipeline" \
+                2> $pipeline/wrench.err
 
-        print_header="--no-header"
+            print_header="--no-header"
 
-        if (( "$VERBOSE" >= 1 )); then
-            echo ""
-            echo "[$($DATE --rfc-3339=ns)] Done. Log written in $pipeline/wrench.err"
-        fi
+            if (( "$VERBOSE" >= 1 )); then
+                echo ""
+                echo "[$($DATE --rfc-3339=ns)] Done. Log written in $pipeline/wrench.err"
+            fi
+        done
     done
+    echo ""
 done
-
 
 echo ""
 echo "[$($DATE --rfc-3339=ns)] Done."
