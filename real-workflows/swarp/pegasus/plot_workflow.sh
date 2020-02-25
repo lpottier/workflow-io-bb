3b1c322fe42de4143aef83ee3a1cb1899eceaf35
Loïc Pottier <lpottier@isi.edu>
2020-02-24 16:55:18 -0800

    started cleaning up the repo
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- real-workflows/swarp/pegasus/plot_workflow.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILE=$1
-NAME=${FILE%.*}
-EXT=${FILE#*.}
-pegasus-graphviz $NAME.dax -o $NAME.dot
-dot -Tpdf -o $NAME.pdf $NAME.dot
-
--- real-workflows/swarp/simple-swarp/start-striped.sh
+++
@@ -1,1 +0,0 @@
-salloc -N 1 -C haswell -q interactive -t 2:00:00 --bbf=bbf-striped.conf
