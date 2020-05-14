0d5dc99d9a7c360ce1905257f319b9981d2e2861
Loïc Pottier <lpottier@isi.edu>
2020-05-14 18:40:59 -0400

    updated SMT 1
    Signed-off-by: Loïc Pottier <lpottier@isi.edu>

--- real-executions/swarp/summit/start-interactive.sh
+++ real-executions/swarp/summit/start-interactive.sh
@@ -6,5 +6,5 @@ PROJECT="CSC355"
 
 # -alloc_flags "NVME" will create a directory /mnt/bb/lpottier/
 
-bsub -Is -P $PROJECT -nnodes 1 -W 1:00 -alloc_flags "NVME" $SHELL
+bsub -Is -P $PROJECT -nnodes 1 -W 1:00 -alloc_flags "nvme smt1" $SHELL
 
